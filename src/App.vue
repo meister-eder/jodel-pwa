df154b03489d7f0b1e93a6455cd036d400c25d37
meister-eder <apfelmusbanane@gmail.com>
2018-01-06 17:29:17 +0100

    added newJodel view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <router-view></router-view>
-  
-    
+
+
   </div>
 </template>
 
@@ -30,5 +30,7 @@ body
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   color: #2c3e50
-  
+
+a
+  text-decoration: none
 </style>
